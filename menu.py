118c92d868dbc074950df1c42e55b273651269a2
christine chemali <christine.chemali@laplateforme.io>
2025-01-22 13:08:03 +0100

    Add: to menu file adding the choice 2 choose a level difficulty and choice 1 random word - to rules files the play_game function

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import display
-from rules import clear_screen, name_is_valid
+from rules import clear_screen, name_is_valid, play_game
 
 # To display the menu
 def menu():
@@ -26,3 +26,26 @@ def menu():
                         display.player_name_issue(e)
                 else:
                         display.invalid_player_name()
+
+        elif choice == "2":
+            clear_screen()
+            print("Choisissez un niveau de difficulté (Plus dur que dur: 1, Dur: 2, Normal: 3, Facile: 4 )")
+            level_choice = input("Entez votre choix (1-4): ")
+            
+            level_mapping = {
+                "1": "Très dur",
+                "2": "dur",
+                "3": "normal",
+                "4": "facile"
+            }
+            chosen_level = level_mapping.get(level_choice)
+            if not chosen_level:
+                print("Choix de niveau invalide, essayez encore.")
+                continue
+            
+            print(f"Bien joué ! Vous avez choisi : {chosen_level.capitalize()}.")
+            word_choice = input("Vous voulez jouer avec un mot au hasard ? (1)\nou ajouter votre mot à notre collection (2) ? (1-2): ")
+            if word_choice == "1":
+                start_return_choice = input("Vous avez choisi au hasard ! Prêt à jouer ? (1 pour jouer, 2 pour le menu): ")
+                if start_return_choice == "1":
+                    play_game(level_choice)
--- rules.py
+++ rules.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 # To clear the terminal screen
 def clear_screen():
@@ -8,3 +9,72 @@ def clear_screen():
 # To validate player name
 def name_is_valid(player_name):
     return len(player_name) >= 4 and player_name.isalpha()
+
+# To play the game
+def play_game(level):
+
+    filename_mapping = {
+        "1": ("word1.txt", 5),
+        "2": ("word2.txt", 6),
+        "3": ("word3.txt", 8),
+        "4": ("word4.txt", 10)
+    }
+    
+    filename, errors_remaining = filename_mapping[level]
+        
+    with open(filename, "r") as word_file:
+        words = [line.strip() for line in word_file.readlines()]
+    
+    solution = random.choice(words)
+    letters_found = ""
+    display = "_ " * len(solution)
+
+    print("*** LE JEU DU PENDU ***")
+    
+    while errors_remaining > 0:
+        print("\nLe mot à deviner : ", display)
+        print(f"Nombre d'erreurs restant : {errors_remaining}")
+        guess = input("Propose une lettre : ")[0:1].lower()
+
+        if guess in solution:
+            letters_found += guess
+            print("Bien joué!")
+        else:
+            errors_remaining -= 1
+            print("Pas cette fois, essaie encore !\n")
+            if errors_remaining == 0:
+                print(" ==========Y= ")
+            if errors_remaining <= 1:
+                print(" ||/       |  ")
+            if errors_remaining <= 2:
+                print(" ||        0  ")
+            if errors_remaining <= 3:
+                print(" ||       /|\ ")
+            if errors_remaining <= 4:
+                print(" ||       /|  ")
+            if errors_remaining <= 5:                    
+                print("==============")
+            if errors_remaining <= 6:
+                print("||  ||  ||  ||")
+            if errors_remaining <= 7:
+                print("==============")
+            if errors_remaining <= 8:
+                print("||  ||  ||  ||")
+            if errors_remaining <= 9:
+                print("==============")
+            if errors_remaining <= 10:
+                print("||  ||  ||  ||")            
+
+        display = ""
+        for current_character in solution:
+            if current_character in letters_found:
+                display += current_character + " "
+            else:
+                display += "_ "
+
+        if "_" not in display:
+            print(f"\n *** Vous avez gagné ! Le mot était : {solution} *** ")
+            break
+    
+    if errors_remaining == 0:
+        print(f"\nVous avez perdu ! : Le mot était : {solution}")
