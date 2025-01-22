a1c958f5b8631dfaf3adb9fb6d4eb837c6186545
christine chemali <christine.chemali@laplateforme.io>
2025-01-22 12:26:53 +0100

    Starting the Hangman Menu - Register - Play - Score tab

---
+++ menu.py
@@ -0,0 +1,12 @@
+
+
+# To display the menu
+def menu():
+    player_name = ""
+    while True:
+        print("\n*** MENU DU PENDU ***")
+        print("1. S'enregistrer")
+        print("2. JOUER ")
+        print("3. Le tableau des scores ")
+        
+        choice = input("\nEntrer votre choix (1-3): ")
